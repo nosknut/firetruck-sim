f0a0dc79b3e4fe8f2d834fcbdfe9e2490337c1a5
nosknut <nosknut@gmail.com>
2023-07-25 17:49:30 +0200

    Polishes joystick component

--- src/routes/Joystick.svelte
+++ src/routes/Joystick.svelte
@@ -5,19 +5,28 @@
 	export let x = 0;
 	export let y = 0;
 
-	let joystickPosition = {
-		x: 0,
-		y: 0
+	export let angle = 0;
+	export let distance = 0;
+
+	export let xPos = 0;
+	export let yPos = 0;
+
+	export let colors = {
+		background: 'transparent',
+		area: 'grey',
+		handle: 'darkgrey'
 	};
 
-	let handlePosition = {
-		x: 0,
-		y: 0
-	};
+	let areaHandle: HTMLDivElement;
 
 	let joystickSize = {
 		width: 0,
 		height: 0
+	};
+
+	$: joystickHalfSize = {
+		width: joystickSize.width / 2,
+		height: joystickSize.height / 2
 	};
 
 	let clicked = false;
@@ -25,58 +34,62 @@
 	$: if (!clicked) {
 		x = 0;
 		y = 0;
+		xPos = 0;
+		yPos = 0;
 	}
-
-	$: xPos = x * (size / 2);
-	$: yPos = -y * (size / 2);
 
 	function onDrag(event: MouseEvent) {
 		if (!clicked) return;
-		let xPos = -(joystickPosition.x - event.clientX) + joystickSize.width * 0.25;
-		let yPos = -(joystickPosition.y - event.clientY) + joystickSize.height * 0.25;
+		let joystickPosition = areaHandle.getBoundingClientRect();
 
-		let distance = Math.sqrt(xPos * xPos + yPos * yPos);
-		let angle = Math.atan2(yPos, xPos);
-		if (distance > size / 2) {
-			xPos = (Math.cos(angle) * size) / 2;
-			yPos = (Math.sin(angle) * size) / 2;
+		xPos = -(joystickPosition.x - event.clientX) - joystickHalfSize.width;
+		yPos = joystickPosition.y - event.clientY + joystickHalfSize.height;
+		distance = Math.sqrt(xPos * xPos + yPos * yPos);
+		angle = Math.atan2(yPos, xPos);
+
+		if (round) {
+			if (distance > size / 2) {
+				xPos = Math.cos(angle) * joystickHalfSize.width;
+				yPos = Math.sin(angle) * joystickHalfSize.height;
+			}
+		} else {
+			if (xPos > joystickHalfSize.width) xPos = joystickHalfSize.width;
+			if (xPos < -joystickHalfSize.width) xPos = -joystickHalfSize.width;
+			if (yPos > joystickHalfSize.height) yPos = joystickHalfSize.height;
+			if (yPos < -joystickHalfSize.height) yPos = -joystickHalfSize.height;
 		}
 
-		x = xPos / (size / 2);
-		y = -yPos / (size / 2);
+		x = xPos / joystickHalfSize.width;
+		y = yPos / joystickHalfSize.height;
 	}
 </script>
 
-<div class="joystick" class:round on:mousemove={onDrag} role="button" tabindex="0">
+<svelte:body on:mouseup={() => (clicked = false)} on:mousemove={onDrag} />
+
+<div class="joystick" class:round style="background-color: {colors.background};">
 	<div
 		class="joystick-area"
-		style="width: {size}px; height: {size}px;"
+		style="width: {size}px; height: {size}px; background-color: {colors.area};"
 		bind:clientWidth={joystickSize.width}
 		bind:clientHeight={joystickSize.height}
-		bind:offsetHeight={joystickPosition.y}
-		bind:offsetWidth={joystickPosition.x}
+		bind:this={areaHandle}
 	>
 		<button
 			class="joystick-handle"
 			on:mousedown={() => (clicked = true)}
-			on:mouseup={() => (clicked = false)}
-			style="width: {size / 2}px; height: {size / 2}px; top: {yPos}px; left: {xPos}px;"
-			bind:offsetHeight={handlePosition.y}
-			bind:offsetWidth={handlePosition.x}
+			style="width: {size / 2}px; height: {size / 2}px; top: {-yPos +
+				joystickHalfSize.height}px; left: {xPos +
+				joystickHalfSize.width}px; transform: translate(-50%, -50%); background-color: {colors.handle};"
 		/>
 	</div>
 </div>
 
 <style>
 	.joystick {
-		padding: 10px;
 		width: min-content;
-		position: relative;
 	}
 	.joystick-area {
-		background-color: blue;
-		display: grid;
-		place-content: center;
+		position: relative;
 	}
 
 	.joystick.round .joystick-area {
@@ -84,7 +97,12 @@
 	}
 
 	.joystick-handle {
-		background-color: darkblue;
 		border-radius: 50%;
+		position: absolute;
+		border: none;
+	}
+
+	.joystick-handle:active {
+		box-shadow: inset 0 0 10px black;
 	}
 </style>
