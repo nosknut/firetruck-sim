59f6bedcd3b9f03c83936a8888881d63780c96fa
nosknut <nosknut@gmail.com>
2023-07-29 21:42:31 +0200

    Removes console.log for serial monitor

--- src/lib/stores/serialMonitor.ts
+++ src/lib/stores/serialMonitor.ts
@@ -7,6 +7,6 @@ export const serialMonitor = writable('');
 serialPort.onReceive((data: { p: number; v: number } | { print: string }) => {
     if ('print' in data) {
         serialMonitor.update((value) => value + data.print)
-        console.log('Serial: ', data.print);
+        // console.log('Serial: ', data.print);
     }
 }).catch(console.error);
