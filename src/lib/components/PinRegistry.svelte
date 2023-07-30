e04ec7c94be406bb422364e3a1a5354b2bcaafca
nosknut <nosknut@gmail.com>
2023-07-30 05:37:00 +0200

    Adds empty table message in pin registry

--- src/lib/components/PinRegistry.svelte
+++ src/lib/components/PinRegistry.svelte
@@ -25,44 +25,48 @@
 </script>
 
 {#if $serialPort.isOpen}
-	<Table shadow color="blue">
-		<TableHead>
-			<TableHeadCell>Pin</TableHeadCell>
-			<TableHeadCell>Value</TableHeadCell>
-			<TableHeadCell>Toggle</TableHeadCell>
-		</TableHead>
-		<TableBody tableBodyClass="divide-y">
-			{#each Object.keys($modes) as pin (pin)}
-				<TableBodyRow color={$pins[pin] ? 'green' : 'blue'}>
-					<TableBodyCell tdClass="px-6 py-1 whitespace-nowrap font-medium">{pin}</TableBodyCell>
-					<TableBodyCell tdClass="px-6 py-1 whitespace-nowrap font-medium">
-						{#if $modes[pin] === 'input'}
-							<Input
-								size="sm"
-								placeholder={$pins[pin]}
-								bind:value={valueInputs[pin]}
-								defaultClass="w-10 h-5 text-center"
-								on:keydown={onEnter(() => setPin(pin))}
-							/>
-						{:else}
-							<div class="text-center">{$pins[pin]}</div>
-						{/if}
-					</TableBodyCell>
-					<TableBodyCell tdClass="px-6 py-1 whitespace-nowrap font-medium">
-						{#if $simPins.has(pin)}
-							<div class="text-center">Sim</div>
-						{:else if $modes[pin] === 'input'}
-							<Toggle
-								size="small"
-								checked={!!$pins[pin]}
-								on:change={() => pins.setPin(pin, !$pins[pin])}
-							/>
-						{:else}
-							<div class="text-center">Output</div>
-						{/if}
-					</TableBodyCell>
-				</TableBodyRow>
-			{/each}
-		</TableBody>
-	</Table>
+	{#if Object.keys($modes).length}
+		<Table shadow color="blue">
+			<TableHead>
+				<TableHeadCell>Pin</TableHeadCell>
+				<TableHeadCell>Value</TableHeadCell>
+				<TableHeadCell>Toggle</TableHeadCell>
+			</TableHead>
+			<TableBody tableBodyClass="divide-y">
+				{#each Object.keys($modes) as pin (pin)}
+					<TableBodyRow color={$pins[pin] ? 'green' : 'blue'}>
+						<TableBodyCell tdClass="px-6 py-1 whitespace-nowrap font-medium">{pin}</TableBodyCell>
+						<TableBodyCell tdClass="px-6 py-1 whitespace-nowrap font-medium">
+							{#if $modes[pin] === 'input'}
+								<Input
+									size="sm"
+									placeholder={$pins[pin]}
+									bind:value={valueInputs[pin]}
+									defaultClass="w-10 h-5 text-center"
+									on:keydown={onEnter(() => setPin(pin))}
+								/>
+							{:else}
+								<div class="text-center">{$pins[pin]}</div>
+							{/if}
+						</TableBodyCell>
+						<TableBodyCell tdClass="px-6 py-1 whitespace-nowrap font-medium">
+							{#if $simPins.has(pin)}
+								<div class="text-center">Sim</div>
+							{:else if $modes[pin] === 'input'}
+								<Toggle
+									size="small"
+									checked={!!$pins[pin]}
+									on:change={() => pins.setPin(pin, !$pins[pin])}
+								/>
+							{:else}
+								<div class="text-center">Output</div>
+							{/if}
+						</TableBodyCell>
+					</TableBodyRow>
+				{/each}
+			</TableBody>
+		</Table>
+	{:else}
+		No pins detected. The controller might not be running or you may have forgotten to run pinMode().
+	{/if}
 {/if}
